147dcc5fd7238d0a89e2ac6aa2d35ae4cb1c72db
zeke-taylor <zeketaylor45@outlook.com>
2017-12-05 10:28:37 -0600

    Update Tokens.lua

--- Tokens.lua
+++ Tokens.lua
@@ -1,4 +1,9 @@
 local Tokens = { }
 Tokens.type = {}
+Tokens.type.int = "integer"
+Tokens.type.number = "number"
+int_type = {}
+int_type[0] = Tokens.type.int
+int_type[1] = Tokens.type.number
 Tokens.type.string = "String"
 local str_type = Tokens.type.string
